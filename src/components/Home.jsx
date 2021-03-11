b5150581932123880e798d947a8cc4ee739cc473
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-03-11 17:20:23 -0500

    maquetacion de la estructura basica del formulario

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,52 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Registro de usuario</h1>
+        <div class="regitro-form">
+            <h3>Registro de usuarios</h3>
+            <form action="">
+                <div >
+                    <label>Tipo de documento:</label>
+                    <select >
+                        <option >--Seleccione--</option>
+                        <option>CC</option>
+                        <option>CE</option>
+                        <option>PP</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Identificación</label>
+                    <input type="number" name="identificacion" placeholder="Identificación"/>
+                </div>
+                <div>
+                    <label>Nombre(s):</label>
+                    <input type="text" name="nombres" placeholder="Nombre(s)"/>
+                </div>
+                <div>
+                    <label>Apellidos:</label>
+                    <input type="text" name="apellidos" placeholder="Apellidos"/>
+                </div>
+                <div >
+                    <label>Nacionalidad:</label>
+                    <select >
+                        <option>--Seleccione--</option>
+                        <option>CC</option>
+                        <option>CE</option>
+                        <option>PP</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Celular</label>
+                    <input type="tel" name="celular" placeholder="Celular" />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Email" />
+                </div>
+                <div>
+                    <button type="submit">+Agregar</button>
+                </div>
+            </form>
         </div>
+
     )
 }
