5baae845a6065bf99c0caef7ce7143341c4d33d2
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-03-11 18:15:58 -0500

    maquetacion del formulario e implementacion de las grid para el diseño responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,58 @@
 import React from 'react'
+import './home.css'
 
 
 
 export default function Home() {
     return (
-        <div class="regitro-form">
+        <div className="content">
             <h3>Registro de usuarios</h3>
-            <form action="">
-                <div >
-                    <label>Tipo de documento:</label>
-                    <select >
-                        <option >--Seleccione--</option>
-                        <option>CC</option>
-                        <option>CE</option>
-                        <option>PP</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Identificación</label>
-                    <input type="number" name="identificacion" placeholder="Identificación"/>
-                </div>
-                <div>
-                    <label>Nombre(s):</label>
-                    <input type="text" name="nombres" placeholder="Nombre(s)"/>
-                </div>
-                <div>
-                    <label>Apellidos:</label>
-                    <input type="text" name="apellidos" placeholder="Apellidos"/>
-                </div>
-                <div >
-                    <label>Nacionalidad:</label>
-                    <select >
-                        <option>--Seleccione--</option>
-                        <option>CC</option>
-                        <option>CE</option>
-                        <option>PP</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Celular</label>
-                    <input type="tel" name="celular" placeholder="Celular" />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" name="email" placeholder="Email" />
-                </div>
-                <div>
-                    <button type="submit">+Agregar</button>
-                </div>
-            </form>
+            <div className="content-form">
+                <form action="">
+                    <p >
+                        <label>Tipo de documento:</label>
+                        <select >
+                            <option >--Seleccione--</option>
+                            <option>CC</option>
+                            <option>CE</option>
+                            <option>PP</option>
+                        </select>
+                    </p>
+                    <p>
+                        <label>Identificación</label>
+                        <input type="number" name="identificacion" placeholder="Identificación" />
+                    </p>
+                    <p>
+                        <label>Nombre(s):</label>
+                        <input type="text" name="nombres" placeholder="Nombre(s)" />
+                    </p>
+                    <p>
+                        <label>Apellidos:</label>
+                        <input type="text" name="apellidos" placeholder="Apellidos" />
+                    </p>
+                    <p >
+                        <label>Nacionalidad:</label>
+                        <select >
+                            <option>--Seleccione--</option>
+                            <option>CC</option>
+                            <option>CE</option>
+                            <option>PP</option>
+                        </select>
+                    </p>
+                    <p>
+                        <label>Celular</label>
+                        <input type="tel" name="celular" placeholder="Celular" />
+                    </p>
+                    <p>
+                        <label>Email</label>
+                        <input type="email" name="email" placeholder="Email" />
+                    </p>
+                    <p>
+                        <button type="submit">+Agregar</button>
+                    </p>
+                </form>
+            </div>
+
         </div>
 
     )
