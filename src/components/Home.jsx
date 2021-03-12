76c96a92a63112caf9a399f2938e7445c8533467
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-03-12 13:08:01 -0500

    se creo la estructura de la tabla junto con los metodos handlechange para capturar la informacion del formulario

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,72 +2,155 @@ import React, { Component } from 'react'
 import './home.css'
 import axios from 'axios'
 
-
+const url = "https://restcountries.eu/rest/v2/all"
 
 class Home extends Component{
-
     state={
-        paises:[]
+        paises:[],
+        form: {
+            tipodoc: '',
+            identificacion: '',
+            nombres: '',
+            apellidos: '',
+            nacionalidad: '',
+            celular: '',
+            email: '',
+            administrador: ''
+        }
+    }
+
+    peticionGet = () => {
+        axios.get(url).then(response=>{
+            // console.log(response);
+            this.setState({paises: response.data})
+        })
+        .catch(error=>{
+            console.log(error)
+        });
     }
 
     componentDidMount(){
-        axios.get("https://restcountries.eu/rest/v2/all")
+        // axios.get("https://restcountries.eu/rest/v2/all")
+        // .then(response=>{
+        //     // console.log(response);
+        //     this.setState({paises: response.data})
+        // })
+        // .catch(error=>{
+        //     console.log(error)
+        // });
+
+        this.peticionGet();
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state)
+        axios.post("http://144.217.88.168:3030/api/user", this.state)
         .then(response=>{
             console.log(response);
             this.setState({paises: response.data})
         })
         .catch(error=>{
             console.log(error)
-        })
+        });
+        
     }
+
+    handleChange = async e => {
+        e.persist();
+        await this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+
+            }
+        });
+        // console.log(this.state.form);
+    }
+
+
+
     render(){
+    const { form } = this.state;
     return (
         <div className="content">
             <h3>Registro de usuarios</h3>
             <div className="content-form">
-                <form action="">
+                <form action="" onSubmit={this.handleSubmit}>
                     <p >
                         <label>Tipo de documento:</label>
-                        <select >
-                            <option >--Seleccione--</option>
-                            <option>CC</option>
-                            <option>CE</option>
-                            <option>PP</option>
+                        <select name="tipodoc" onChange={this.handleChange}>
+                            <option>--Seleccione--</option>
+                            <option  value="CC">CEDULA CIUDADANIA</option>
+                            <option  value="CE">CEDULA EXTRANJERIA</option>
+                            <option  value="PP">PASAPORTE</option>
                         </select>
                     </p>
                     <p>
                         <label>Identificación</label>
-                        <input type="number" name="identificacion" placeholder="Identificación"  />
+                        <input type="number" name="identificacion" placeholder="Identificación" value={form.identificacion} onChange={this.handleChange} />
                     </p>
                     <p>
                         <label>Nombre(s):</label>
-                        <input type="text" name="nombres" placeholder="Nombre(s)" />
+                        <input type="text" name="nombres" placeholder="Nombre(s)" value={form.nombres} onChange={this.handleChange}/>
                     </p>
                     <p>
                         <label>Apellidos:</label>
-                        <input type="text" name="apellidos" placeholder="Apellidos" />
+                        <input type="text" name="apellidos" placeholder="Apellidos" value={form.apellidos} onChange={this.handleChange}/>
                     </p>
                     <p >
                         <label>Nacionalidad:</label>
-                        <select name="paises" >
+                        <select name="nacionalidad"  onChange={this.handleChange}>
+                            <option >--Seleccione--</option>
                             {this.state.paises.map(elemento=>(
-                                <option key={elemento.numericCode} value={elemento.numericCode}>{elemento.name}</option>
+                                <option key={elemento.numericCode} value={form.numericCode} >{elemento.name}</option>
                             )
                             )}
                         </select>
                     </p>
                     <p>
                         <label>Celular</label>
-                        <input type="tel" name="celular" placeholder="Celular" />
+                        <input type="number" name="celular" placeholder="Celular" value={form.celular} onChange={this.handleChange}/>
                     </p>
                     <p>
                         <label>Email</label>
-                        <input type="email" name="email" placeholder="Email" />
+                        <input type="email" name="email" placeholder="Email" value={form.email} onChange={this.handleChange}/>
                     </p>
                     <p>
-                        <button type="submit">+Agregar</button>
+                        <label>Administrador</label>
+                        <input type="text" name="administrador" placeholder="Administrador" value={form.administrador} onChange={this.handleChange} />
+                    </p>
+                    <p className="block">
+                        <button  type="submit">+Agregar</button>
                     </p>
                 </form>
+            </div>
+            <div className="content-table">
+                <table className="table" border="1">
+                    <thead>
+                        <tr>
+                            <th>Tipo Documento</th>
+                            <th>Documento</th>
+                            <th>Nombre Completo</th>
+                            <th>Nacionalidad</th>
+                            <th>Celular</th>
+                            <th>Email</th>
+                            <th>Administrador</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>cedula</td>
+                            <td>1092334126</td>
+                            <td>keymer jimenez</td>
+                            <td>colombia</td>
+                            <td>3014534664</td>
+                            <td>keimer@gmail.com</td>
+                            <td>keymer</td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
             </div>
 
         </div>
